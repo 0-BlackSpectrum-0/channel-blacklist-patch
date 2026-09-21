5644496b457b29cad2799c3c2cbc19cb422f0cab
0xrxL <127248639+0xrxL@users.noreply.github.com>
2026-09-21 18:56:29 +0200

    fix(YouTube - Comments filter): Restored chat button page indicator

--- extensions/youtube/src/main/java/app/morphe/extension/youtube/patches/components/CommentsFilter.java
+++ extensions/youtube/src/main/java/app/morphe/extension/youtube/patches/components/CommentsFilter.java
@@ -56,7 +56,6 @@ public class CommentsFilter extends Filter {
     private final StringFilterGroup comments;
     private final StringFilterGroup commentsFilterBar;
     private final StringFilterGroup emojiButton;
-    private final StringFilterGroup previewCommentDotsSelector;
 
     public CommentsFilter() {
         var channelGuidelines = new StringFilterGroup(
@@ -145,11 +144,6 @@ public class CommentsFilter extends Filter {
                 "comments_entry_point_simplebox"
         );
 
-        previewCommentDotsSelector = new StringFilterGroup(
-                Settings.HIDE_COMMENTS_PREVIEW_COMMENT,
-                VIDEO_METADATA_CAROUSEL_PATH
-        );
-
         var thanksButton = new StringFilterGroup(
                 Settings.HIDE_COMMENTS_THANKS_BUTTON,
                 "super_thanks_button.e"
@@ -179,7 +173,6 @@ public class CommentsFilter extends Filter {
                 emojiButton,
                 giftAnimationAndCards,
                 previewComment,
-                previewCommentDotsSelector,
                 thanksButton,
                 timestampButton,
                 topFansButton
@@ -216,10 +209,6 @@ public class CommentsFilter extends Filter {
 
         if (matchedGroup == commentsFilterBar) {
             return Settings.HIDE_FILTER_BAR_IN_COMMENTS.get() && PlayerType.getCurrent().isMaximizedOrFullscreen();
-        }
-
-        if (matchedGroup == previewCommentDotsSelector) {
-            return path.contains("carousel_header") && path.endsWith("|ContainerType|ContainerType|ContainerType|");
         }
 
         return true;
