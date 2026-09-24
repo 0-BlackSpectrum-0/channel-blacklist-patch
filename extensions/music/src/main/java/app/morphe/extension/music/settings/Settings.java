cfc880bbba4b2ef9451c537fcdb4751e2ce09caa
mrhard9090 <74858909+mrhard9090@users.noreply.github.com>
2026-09-24 15:02:42 +0300

    feat(YouTube Music - Hide buttons): Add "Hide Voice search button" and "Hide Sound search button" (#3250)

--- extensions/music/src/main/java/app/morphe/extension/music/settings/Settings.java
+++ extensions/music/src/main/java/app/morphe/extension/music/settings/Settings.java
@@ -49,6 +49,8 @@ public class Settings extends SharedYouTubeSettings {
     public static final BooleanSetting HIDE_FILTER_BAR = new BooleanSetting("morphe_music_hide_filter_bar", FALSE, true);
     public static final BooleanSetting HIDE_HISTORY_BUTTON = new BooleanSetting("morphe_music_hide_history_button", FALSE, true);
     public static final BooleanSetting HIDE_SEARCH_BUTTON = new BooleanSetting("morphe_music_hide_search_button", FALSE, true);
+    public static final BooleanSetting HIDE_VOICE_SEARCH_BUTTON = new BooleanSetting("morphe_music_hide_voice_search_button", FALSE, true);
+    public static final BooleanSetting HIDE_SOUND_SEARCH_BUTTON = new BooleanSetting("morphe_music_hide_sound_search_button", FALSE, true);
     public static final BooleanSetting HIDE_NOTIFICATION_BUTTON = new BooleanSetting("morphe_music_hide_notification_button", FALSE, true);
     public static final BooleanSetting HIDE_PODCAST_EPISODE_DOWNLOAD_BUTTON = new BooleanSetting("morphe_music_hide_podcast_episode_download_button", FALSE, true);
     public static final BooleanSetting HIDE_NAVIGATION_BAR = new BooleanSetting("morphe_music_hide_navigation_bar", FALSE, true);
